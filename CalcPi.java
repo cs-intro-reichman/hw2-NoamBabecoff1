184d103e9e45ec46ab091aea0b7611ae627026f0
NoamBabecoff1 <noam.babecoff@post.runi.ac.il>
2025-11-13 11:12:36 +0200

    HW FIX4

--- CalcPi.java
+++ CalcPi.java
@@ -1,20 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    int num = Integer.parseInt(args[0]);
-		double sum = 0; 
+	public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        double sum = 0;
 
-		for (int i = 0; i < num; i++) {
-			if(i%2 == 0){
-				sum += (double)1 / (i * 2 + 1);
-			}
-			else{
-				sum -= (double)1 / (i * 2 + 1);
-			}
-		}
-		sum = sum*4;
-		
-		System.err.println("pi according to java: " + Math.PI);
-		System.err.println("pi, approximated:   " + sum);
-	}
+        for (int i = 0; i < num; i++) {
+            if (i % 2 == 0) {
+                sum += 1.0 / (2 * i + 1);
+            } else {
+                sum -= 1.0 / (2 * i + 1);
+            }
+        }
+
+        sum *= 4;
+
+        System.out.println("pi according to Java: " + Math.PI);
+        // 5 spaces
+		System.out.println("pi, approximated:     " + sum);
+    }
 }
